0579103b426a88b378f3c7b3a3c72b4fd06b070c
Joseph Tams <jtams322@gmail.com>
2025-07-24 11:41:52 -0700

    Fixed mason

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -102,31 +102,6 @@ return {
 			-- vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
 		end
 
-		require("mason").setup()
-		require("mason-lspconfig").setup()
-
-		local servers = {}
-
-		local capabilities = vim.lsp.protocol.make_client_capabilities()
-		capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
-
-		local mason_lspconfig = require("mason-lspconfig")
-
-		mason_lspconfig.setup({
-			ensure_installed = vim.tbl_keys(servers),
-		})
-
-		mason_lspconfig.setup_handlers({
-			function(server_name)
-				require("lspconfig")[server_name].setup({
-					capabilities = capabilities,
-					on_attach = on_attach,
-					settings = servers[server_name],
-					filetypes = (servers[server_name] or {}).filetypes,
-				})
-			end,
-		})
-
 		local cmp = require("cmp")
 		local luasnip = require("luasnip")
 		require("luasnip.loaders.from_vscode").lazy_load()
