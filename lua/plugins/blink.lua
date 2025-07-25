cf2dc703fd214135bdf93c8b7d2e37614ee6d007
Joseph Tams <jtams322@gmail.com>
2025-07-25 11:39:15 -0700

    added docs to cmp

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -63,15 +63,11 @@ return { -- Autocompletion
 		},
 
 		appearance = {
-			-- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-			-- Adjusts spacing to ensure icons are aligned
 			nerd_font_variant = "mono",
 		},
 
 		completion = {
-			-- By default, you may press `<c-space>` to show the documentation.
-			-- Optionally, set `auto_show = true` to show the documentation after a delay.
-			documentation = { auto_show = false, auto_show_delay_ms = 500 },
+			documentation = { auto_show = true, auto_show_delay_ms = 400 },
 		},
 
 		sources = {
