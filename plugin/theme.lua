34d411b9404d99abbf2f62bd374fa00ed248be27
Joseph Tams <jtams322@gmail.com>
2026-07-14 15:57:46 -0700

    switched to nord theme

--- plugin/theme.lua
+++ plugin/theme.lua
@@ -1,5 +1,19 @@
 vim.pack.add({
-	"https://github.com/catppuccin/nvim",
+	-- "https://github.com/catppuccin/nvim",
+	"https://github.com/shaunsingh/nord.nvim",
 })
 
-vim.cmd.colorscheme("catppuccin-mocha")
+vim.api.nvim_create_autocmd("ColorScheme", {
+	pattern = "*",
+	callback = function()
+		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+		vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+		vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+		vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
+		vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
+	end,
+})
+
+-- vim.cmd.colorscheme("catppuccin-mocha")
+vim.cmd.colorscheme("nord")
